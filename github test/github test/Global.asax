b125940bfcccbc9326e750489d6d54452ca707a5
rbgrandcircus <ruthlbyrd@gmail.com>
2014-06-25 09:40:52 -0700

    First Commit
    First Commit Description

---
+++ github test/github test/Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="github_test.MvcApplication" Language="C#" %>
---
+++ github test/github test/Startup.cs
@@ -0,0 +1,14 @@
+﻿using Microsoft.Owin;
+using Owin;
+
+[assembly: OwinStartupAttribute(typeof(github_test.Startup))]
+namespace github_test
+{
+    public partial class Startup
+    {
+        public void Configuration(IAppBuilder app)
+        {
+            ConfigureAuth(app);
+        }
+    }
+}
